985e82d4b2595529639df0913826208183bdb795
jtljac <jtljac2@gmail.com>
2019-02-15 22:24:42 +0000

    Update Read.py

--- Read.py
+++ Read.py
@@ -2,5 +2,5 @@ import RPi.GPIO as GPIO
 import EasyMFRC522
 
 reader = EasyMFRC522.EasyMFRC522()
-sector = int(input("Enter sector to visit: "))
+sector = int(raw_input("Enter sector to visit: "))
 print(reader.read(sector))
