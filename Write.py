b89635dfd2f38d3d14dd04b5a62ed2db00f047e0
jtljac <jtljac2@gmail.com>
2019-02-15 22:25:44 +0000

    Update Write.py

--- Write.py
+++ Write.py
@@ -4,6 +4,6 @@ import EasyMFRC522
 reader = EasyMFRC522.EasyMFRC522()
 
 sector = int(input("Enter sector to write to: "))
-data = int(input("Enter data to write: "))
+data = raw_input("Enter data to write: ")
 reader.write(data, sector)
 reader.read(sector)
