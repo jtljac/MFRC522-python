994f52586029aa420621d8f028fa3ba80505366f
jtljac <jtljac2@gmail.com>
2019-02-15 22:23:47 +0000

    Create Write.py

---
+++ Write.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+import EasyMFRC522
+
+reader = EasyMFRC522.EasyMFRC522()
+
+sector = int(input("Enter sector to write to: "))
+data = int(input("Enter data to write: "))
+reader.write(data, sector)
+reader.read(sector)
